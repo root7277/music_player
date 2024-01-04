ea7b53368b17ec28d3e97c07003450d1ff2e1385
network7277 <husanjurayev7277@gmail.com>
2024-01-04 10:02:37 +0500

    Enter Page added

---
+++ lib/pages/enter_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class EnterPage extends StatefulWidget {
+  const EnterPage({super.key});
+
+  @override
+  State<EnterPage> createState() => _EnterPageState();
+}
+
+class _EnterPageState extends State<EnterPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
